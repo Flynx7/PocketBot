11d03eb69d150969f6413a96e3fed046eddaba73
Muhammad Umair Kalwar <68455102+UmairGamer0@users.noreply.github.com>
2020-07-18 19:49:19 -0700

    info1

--- commands/info.js
+++ commands/info.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'info',
     description: "bot info",
     execute(message, args){
-        message.channel.send('Hello My Name Is PocketBot Im A Mooderation Discord Bot Im Created By UmairGamerYT');
+        message.channel.send('Hello My Name Is PocketBot Im A Moderation Discord Bot Im Created By UmairGamerYT He Is Best Developer I Hope He Add New Features On Me :)');
     }
 }
