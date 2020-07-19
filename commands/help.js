c80ba913c4f027719e4a14d8365260f8538e5509
Muhammad Umair Kalwar <68455102+UmairGamer0@users.noreply.github.com>
2020-07-18 20:14:07 -0700

    ping1

--- commands/help.js
+++ commands/help.js
@@ -6,10 +6,9 @@ module.exports = {
  
     // Check if the bot's user was tagged in the message
     if (receivedMessage.content.includes(client.user.toString())) {
-        receivedMessage.channel.send("You need help with" + ": " + receivedMessage.content)
+        receivedMessage.channel.send("You need help with" + ":" + receivedMessage.content)}
             const args = message.content.slice(prefix.length).split(/ +/);
             const command = args.shift().toLowerCase();
  
     }
 }
-// wait i restart it on online website after change we need to restart from website
