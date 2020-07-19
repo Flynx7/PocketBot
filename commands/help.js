b162b1a1f2bd35f3313276bdf8ad023ad14cb404
Muhammad Umair Kalwar <68455102+UmairGamer0@users.noreply.github.com>
2020-07-18 20:23:11 -0700

    both1

--- commands/help.js
+++ commands/help.js
@@ -4,11 +4,12 @@ module.exports = {
     execute(message, args){
         message.channel.send('What do you need help with?');
  
-    // Check if the bot's user was tagged in the message
+
     if (receivedMessage.content.includes(client.user.toString())) {
-        receivedMessage.channel.send("You need help with" + ":" + receivedMessage.content)}
-            const args = message.content.slice(prefix.length).split(/ +/);
-            const command = args.shift().toLowerCase();
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const command = args.shift().toLowerCase();
+    receivedMessage.channel.send("You need help with" + ":" + receivedMessage.content.args)}
+
  
     }
 }
