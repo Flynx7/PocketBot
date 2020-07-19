3570c2d0c29a4dd6515bedc9824589b4f163ad67
Muhammad Umair Kalwar <68455102+UmairGamer0@users.noreply.github.com>
2020-07-18 20:10:42 -0700

    ping

--- commands/help.js
+++ commands/help.js
@@ -4,9 +4,12 @@ module.exports = {
     execute(message, args){
         message.channel.send('What do you need help with?');
  
-    receivedMessage.channel.send("Message received from " + receivedMessage.author.toString() + ": " + receivedMessage.content)
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+    // Check if the bot's user was tagged in the message
+    if (receivedMessage.content.includes(client.user.toString())) {
+        receivedMessage.channel.send("You need help with" + ": " + receivedMessage.content)
+            const args = message.content.slice(prefix.length).split(/ +/);
+            const command = args.shift().toLowerCase();
  
     }
 }
+// wait i restart it on online website after change we need to restart from website
