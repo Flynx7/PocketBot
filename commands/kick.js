7c449ec0b286b6d6889f6368cf755660bfbd75c4
Muhammad Umair Kalwar <68455102+UmairGamer0@users.noreply.github.com>
2020-07-18 20:32:47 -0700

    ki1

--- commands/kick.js
+++ commands/kick.js
@@ -1,7 +1,9 @@
+const { GuildMember } = require("discord.js");
+
 module.exports = {
     name: 'kick',
     description: "Kick Peoples From Server!",
     execute(message, args){
-        message.channel.send('User Was Successfully Kicked From Server');
+        message.reply('User Was Successfully Kicked From Server');
     }
 }
